a821607523fbd67b920c002de5cf7f14e7a53cf4
isavalenzuela <isadoravalenzuelafierro@gmail.com>
2022-10-24 12:29:03 -0300

    refactorize
    move login to components folder

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App'
 import './index.css'
-import Login from './Login'
+import Login from './components/Login'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
