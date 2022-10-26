6a11d3cedcebdf36cb9fe026563846056a6653ca
bryan vera <bveracachay@gmail.com>
2022-10-26 09:33:05 -0500

    subiendo solo footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App font-fibraBold bg-fondoMorado min-h-screen">
-      <Header />
-      <Main />
       <Footer />
     </div>
   );
