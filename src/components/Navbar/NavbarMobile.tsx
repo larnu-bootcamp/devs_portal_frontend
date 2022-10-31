63258000dbf4a01871b3f5d0f6ce3c9109c6e155
Toni-dev <antonioraweb@gmail.com>
2022-10-31 14:41:33 -0600

    Se añade navbar

---
+++ src/components/Navbar/NavbarMobile.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Logo from "../../assets/Logo-Morado.svg"
+import IconMenu from "../../assets/menu.png"
+import IconClose from "../../assets/x.png" 
+import "./navbarMobile.css"
+import { useState } from "react"
+
+
+const NavbarMobile = () => {
+
+  const [navbarOpen, setNavbarOpen] = useState(false)
+
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen)
+  }
+
+return (
+  <div className='navbar--mobile'>
+    <div className='navbar--mobile__menu'>
+      <div className='navbar--mobile__logo'>
+        <img src= {Logo} alt="Logo LarnU" />
+      </div>
+      <div className='navbar--mobile__iconMenu'>
+        <button onClick={handleToggle}>{navbarOpen ? (<img src={IconClose}></img>):(<img src={IconMenu}></img>)}</button>
+      </div>
+    </div>
+    <div className={`menuNav ${navbarOpen ? "navbar--mobile__links" : "hidden"}`}>
+      <a href="#">Bootcamp</a>
+      <a href="#">Blog</a>
+      <a href="#">Egresados</a>
+      <a href="#">Mision</a>
+      <button>Registrarse</button>
+    </div>
+  </div>
+)
+}
+
+export default NavbarMobile;
